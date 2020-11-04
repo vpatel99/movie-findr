54769fa3a3479f970d4c05b4208a261be89646ae
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2020-11-03 20:44:28 -0500

    create the boiler plate

---
+++ movie-findr/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-findr/src/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <div className="header__nav_logo">
+          <Link to="/">
+            <img className="header__logo" src="./bikefindr.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="spacer"></div>
+        <div className="header__nav__options">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="log">log in</div>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="sign">create account</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
